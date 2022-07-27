93d676309b013d3905ee492548bc3581f4147ae0
Sophie <wenxi1988@hotmail.com>
2022-07-26 22:39:36 -0400

    Implementing Programmatic (Imperative) Navigation)

--- .history/src/pages/NewQuote_20220726222935.js
+++ .history/src/pages/NewQuote_20220726222935.js
@@ -2,13 +2,9 @@ import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
-  const history = useHistory();
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
   };
-
-  // push new page,will allow to go back
-  history.push("/quotes");
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
 };
